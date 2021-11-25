2f6cece2a81cdb8c09d5768a496ba6fb6cd77ce2
Daan Meijer <daansander02@outlook.com>
2021-11-25 15:42:46 +0100

    Clean up repo

--- Air-Tester/src/mem_leak.c
+++
@@ -1,9 +0,0 @@
-#include "mem_leak.h"
-#include <stdlib.h>
-#include <stdio.h>
-
-void function_that_leaks() {
-	char* v = malloc(5);
-//	free(v);
-	(void)v;
-}
--- Air-Tester/src/mem_leak.h
+++
@@ -1,6 +0,0 @@
-#ifndef MEM_LEAK_H
-#define MEM_LEAK_H
-
-void function_that_leaks();
-
-#endif //MEM_LEAK_H
