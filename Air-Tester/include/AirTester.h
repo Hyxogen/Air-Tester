195e60d6e554332ff81750bcbf01a306010be473
Daan Meijer <daansander02@outlook.com>
2021-11-25 11:07:06 +0100

    Add concept of test groups

--- Air-Tester/include/AirTester.h
+++ Air-Tester/include/AirTester.h
@@ -1,6 +1,34 @@
 #pragma once
 
 #include <vector>
+#include <string>
+
+class Test;
+class TestGroup;
+
+typedef std::vector<Test*> TestList;
+typedef std::vector<TestGroup*> GroupList;
+
+class TestGroup {
+	int			m_FailedTests;
+	TestList*	m_Tests;
+public:
+	std::string m_Name;
+
+	TestGroup(std::string name);
+	~TestGroup();
+
+	void RunAllTests();
+
+	void AddTest(Test* test);
+
+	int GetTestCount() const;
+
+	inline int GetFailedTests() { return m_FailedTests;	}
+
+private:
+	void CleanUp();
+};
 
 class Test {
 public:
@@ -13,21 +41,26 @@ public:
 
 class AirTester {
 private:
-	std::vector<Test*>* m_Tests;
+	GroupList*	m_Groups;
 
 	AirTester();
 	~AirTester();
 public:
 	void RunAll();
 
-	Test* AddTest(Test* test);
+	TestGroup* GetTestGroup(std::string name);
 
 	static AirTester* GetInstance();
+
+private:
+	TestGroup* AddTestGroup(TestGroup* testGroup);
 };
 
 template<class Type>
-Test* RegisterTest() {
-	return AirTester::GetInstance()->AddTest(new Type);
+Test* RegisterTest(std::string groupName) {
+	Test* test = new Type;
+	AirTester::GetInstance()->GetTestGroup(groupName)->AddTest(test);
+	return test;
 }
 
 #define TEST_CLASS_NAME(group, unit) test_##group##_unit
@@ -40,5 +73,5 @@ public:\
 	TEST_CLASS_NAME(group, unit)() = default;\
 	void TestBody() override;\
 };\
-Test const *TEST_CLASS_NAME(group, unit)::test_executor = RegisterTest<TEST_CLASS_NAME(group, unit)>();\
+Test const *TEST_CLASS_NAME(group, unit)::test_executor = RegisterTest<TEST_CLASS_NAME(group, unit)>(#group);\
 void TEST_CLASS_NAME(group, unit)::TestBody()
--- Air-Tester/src/AirTester.cpp
+++ Air-Tester/src/AirTester.cpp
@@ -1,25 +1,31 @@
 #include "AirTester.h"
 
 AirTester::AirTester() {
-	m_Tests = new std::vector<Test*>();
+	m_Groups = new GroupList();
 }
 
 AirTester::~AirTester() {
-	delete m_Tests;
+	delete m_Groups;
 }
 
 void AirTester::RunAll() {
-	std::vector<Test*>::iterator it;
-	
-	for (Test* test : *m_Tests) {
-		test->TestBody();
-		delete test;
+	for (TestGroup* group : *m_Groups) {
+		group->RunAllTests();
 	}
 }
 
-Test* AirTester::AddTest(Test* test) {
-	m_Tests->push_back(test);
-	return test;
+TestGroup* AirTester::GetTestGroup(std::string name) {
+	for (TestGroup* group : *m_Groups) {
+		if (group->m_Name == name)
+			return group;
+	}
+	//Create test group if didn't already exist
+	return AddTestGroup(new TestGroup(name));
+}
+
+TestGroup* AirTester::AddTestGroup(TestGroup* testGroup) {
+	m_Groups->push_back(testGroup);
+	return (testGroup);
 }
 
 AirTester* AirTester::GetInstance() {
@@ -29,10 +35,37 @@ AirTester* AirTester::GetInstance() {
 	return instance;
 }
 
+TestGroup::TestGroup(std::string name) : m_Name(name) {
+	m_Tests = new TestList();
+}
+
+TestGroup::~TestGroup() {
+	CleanUp();
+	delete m_Tests;
+}
+
+void TestGroup::RunAllTests() {
+	for (Test* test : *m_Tests)
+		test->TestBody();
+}
+
+void TestGroup::AddTest(Test* test) {
+	m_Tests->push_back(test);
+}
+
+int TestGroup::GetTestCount() const {
+	return m_Tests->size();
+}
+
+void TestGroup::CleanUp() {
+	for (Test* test : *m_Tests)
+		delete test;
+}
+
 Test::Test() {
 
 }
 
 Test::~Test() {
-	
+
 }
